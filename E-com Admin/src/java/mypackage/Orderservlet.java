147043388cf724d02d8732a400b35ae5459a8f1e
SahanDias <sahankaushalya1102@gmail.com>
2024-04-25 13:38:28 +0530

    create order table's java files
    create order java module helper class
    create order java controller servlet file
    create order java module class

---
+++ E-com Admin/src/java/mypackage/Orderservlet.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author User
+ */
+public class Orderservlet extends HttpServlet {
+
+    
+}
