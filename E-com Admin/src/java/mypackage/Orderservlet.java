16d76ed8ff9571c3bd85dbe2655623a5ab7215a4
SahanDias <sahankaushalya1102@gmail.com>
2024-04-25 13:48:31 +0530

    create conscrutors and getters and setters method in order.java

--- E-com Admin/src/java/mypackage/Orderservlet.java
+++ E-com Admin/src/java/mypackage/Orderservlet.java
@@ -8,6 +8,7 @@ package mypackage;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author User
  */
+@WebServlet("/Orderservlet")
 public class Orderservlet extends HttpServlet {
 
     
